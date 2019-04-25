04f25f655f8881382bd28eb2deddde54a56c57b8
SaberSz <32420868+SaberSz@users.noreply.github.com>
2019-04-26 00:37:23 +0530

    preprocess done. webiste changes

--- Code/app.py
+++ Code/app.py
@@ -1,12 +1,29 @@
 from PIL import Image
 import socket
 import os
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, session
+from pre import pre_process_me
 app = Flask(__name__)
 
 UPLOAD_FOLDER = os.path.basename('uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+MASK_FOLDER = os.path.basename('masks')
+app.config['MASK_FOLDER'] = MASK_FOLDER
+
+#   TODO:
+#   MASK UPLOAD IN WEBSITE, PREDICT BUTTON
+#   MODEL model-tgs-salt-2.h5
+
+
+@app.before_request
+def make_session_active():
+    session.modified = True
+
+
+@app.before_request
+def make_session_permanent():
+    session.permanent = True
+    app.permanent_session_lifetime = timedelta(minutes=300)
 
 
 @app.route('/')
@@ -14,28 +31,62 @@ def hello_world():
     return render_template('index.html')
 
 
-@app.route('/upload', methods=['POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
     if request.method == 'POST':
-        file = request.files['image']
-        f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-        file.save(f)
-        try:
-            im = Image.open(f)
-            prediction = "Image Uploaded"
-        except IOError as e:
-            prediction = f"<i>{e}</i>"
-            os.remove(f)
+        if get_mask() and get_image():
+            prediction = "Uploaded Successfully"
+        else:
+            prediction = "Error :("
         html = f'''<h3>Hello {os.getenv("NAME", "world")}!</h3>
              <b>Hostname:</b> {socket.gethostname()}<br/>
              <b>Message:</b> {prediction}<br/>'''
-        im.thumbnail((101,101))
-        im.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename.split(".")[0]+".PNG"))
-        del im
 
     return html
 
 
+@app.route('/', methods=['POST'])
+def upload_file():
+    if 'mask_file_name' in session and 'image_file_name' in session:
+        X, X_feat, Y = pre_process_me(session['image_file_name'], session['mask_file_name'])
+        #call in model and predict
+
+    return html
+
+
+def get_image():
+    file = request.files['image']
+    f = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+    file.save(f)
+    flag = False
+    try:
+        im = Image.open(f)
+        flag = True
+        session['image_file_name'] = file.filename
+    except IOError as e:
+        os.remove(f)
+    im.thumbnail((101, 101))
+    im.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename.split(".")[0] + ".PNG"))
+    del im
+    return flag
+
+
+def get_mask():
+    file = request.files['mask']
+    f = os.path.join(app.config['MASK_FOLDER'], file.filename)
+    file.save(f)
+    flag = False
+    try:
+        im = Image.open(f)
+        flag = True
+        session['mask_file_name'] = file.filename
+    except IOError as e:
+        os.remove(f)
+    im.thumbnail((101, 101))
+    im.save(os.path.join(app.config['MASK_FOLDER'], file.filename.split(".")[0] + ".PNG"))
+    del im
+    return flag
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
-
---
+++ Code/pre.py
@@ -0,0 +1,45 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.transform import resize
+import numpy as np
+im_width = 128
+im_height = 128
+border = 5
+im_chan = 2
+n_features = 1
+
+
+def pre_process_me(id_image, id_mask):
+
+    # Get and resize train images and masks
+
+    X = np.zeros((im_height, im_width, im_chan), dtype=np.float32)
+    y = np.zeros((im_height, im_width, 1), dtype=np.float32)
+    X_feat = np.zeros((n_features), dtype=np.float32)
+
+    try:
+
+        img = load_img('D:\\Github Repos\\Hydrocarbon-Exploration-using-Seismic-Imaging\\Code\\uploads\\' + id_image, grayscale=True)
+        x_img = img_to_array(img)
+        x_img = resize(x_img, (128, 128, 1), mode='constant', preserve_range=True)
+
+        # Create cumsum x
+        x_center_mean = x_img[border:-border, border:-border].mean()
+        x_csum = (np.float32(x_img) - x_center_mean).cumsum(axis=0)
+        x_csum -= x_csum[border:-border, border:-border].mean()
+        x_csum /= max(1e-3, x_csum[border:-border, border:-border].std())
+
+        # Load Y
+        mask = img_to_array(load_img('D:\\Github Repos\\Hydrocarbon-Exploration-using-Seismic-Imaging\\Code\\masks\\' + id_mask, grayscale=True))
+        mask = resize(mask, (128, 128, 1), mode='constant', preserve_range=True)
+
+        # Save images
+        X[..., 0] = x_img.squeeze() / 255
+        X[..., 1] = x_csum.squeeze()
+        y = mask / 255
+
+        return X, X_feat, y
+    except Exception as e:
+        raise e
+
+
+print(pre_process_me("000e218f21.png", "000e218f21.png"))
